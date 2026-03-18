bd828ac85a4a96834165ab415d5c27538e2d784f
petergolde <peter@golde.org>
2026-03-18 07:56:56 -0700

    Add Avalonia app from template.

---
+++ src/AvPurplePen/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AvPurplePen.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ src/AvPurplePen/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvPurplePen.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
